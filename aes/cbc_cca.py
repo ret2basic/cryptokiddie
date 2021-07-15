5ed503bb03325d853b65a906e7b47df54855e1f5
ret2basic <ret2basic.com>
2021-07-14 20:27:11 -0400

    Pwnie Island rocks

--- aes/cbc_cca.py
+++ aes/cbc_cca.py
@@ -9,6 +9,15 @@ def cbc_cca(cc: bytes, mm: bytes):
 
 	- cc: a byte string containing two identical blocks as ciphertext
 	- mm: a byte string containing the output from the decryption oracle
+
+	Output:
+
+	- iv: the initialization vector recovered from the CBC CCA attack
+
+	Caution:
+
+	The ciphertext cc must contain two identical blocks. Two 16-byte blocks containing
+	only null bytes are recommended.
 	"""
 	assert len(cc) == 32, "The first argument must be a 32-byte byte string."
 	assert cc[:16] == cc[16:], "The first argument must have the same block 1 and block 2."
@@ -16,4 +25,6 @@ def cbc_cca(cc: bytes, mm: bytes):
 	p0 = mm[:16]
 	p1 = mm[16:]
 
-	return xor(xor(p0, p1), cc[:16])
+	iv = xor(xor(p0, p1), cc[:16])
+
+	return iv
